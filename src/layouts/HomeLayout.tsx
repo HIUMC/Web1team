bedde9837a2b85a0e0927322821479e965383c26
jangmk05 <jangmk05@gmail.com>
2026-01-08 14:15:20 +0900

    [FEAT] home layout main부분 스타일 수정

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,16 +1,19 @@
 // layouts/AuthLayout.tsx
-import { Outlet } from 'react-router-dom'
-import { Header } from '../components/common/Header'
-import SideBar from '../components/common/SideBar'
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/common/Header";
+import SideBar from "../components/common/SideBar";
 
 export const HomeLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <SideBar/>
-      <main className="flex-1 flex justify-center items-center">
-        <Outlet />
-      </main>
+      <div className="flex flex-1">
+        <SideBar />
+
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     </div>
-  )
-}
+  );
+};
