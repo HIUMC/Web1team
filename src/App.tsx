22575158a150a48f000e07e5c474d671a18d6d8f
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-01-08 12:15:35 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { createBrowserRouter, RouterProvider, type RouteObject } from 'react-router-dom'
 import './App.css'
-import { Header } from './components/layout/Header'
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { ProtectedLayout } from './layouts/ProtectedLayout';
+import { HomeLayout } from './layouts/HomeLayout';
+
+const publicRoustes: RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      { index: true, element: <HomePage/> },
+      { path: "login", element: <LoginPage/> },
+    ]
+  }
+];
+
+const protectedRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <ProtectedLayout />,
+    children: [
+    ],
+  },
+];
+const router = createBrowserRouter([...publicRoustes, ...protectedRoutes]);
 
 function App() {
-
   return (
-    <>
-      <Header/>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
