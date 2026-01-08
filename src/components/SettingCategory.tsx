9ad08f80ca88cca0d607f9fd4f0f401f1b51d8ef
jangmk05 <jangmk05@gmail.com>
2026-01-08 17:08:16 +0900

    [REFACTOR] 설정페이지 카테고리 컴포넌트 분리

---
+++ src/components/SettingCategory.tsx
@@ -0,0 +1,24 @@
+import trashBinGray from "../assets/icons/trashBinGray.svg";
+
+interface CategoryProps {
+  icon: string;
+  title: string;
+  alt: string;
+}
+
+export default function SettingCategory({ icon, title, alt }: CategoryProps) {
+  return (
+    <div className="w-60 px-7 py-6 bg-white rounded-lg outline outline-1 outline-offset-[-1px] outline-sky-300 inline-flex flex-col justify-start items-start gap-2.5">
+      <div className="self-stretch inline-flex justify-between items-center">
+        <div className="flex justify-start items-center gap-3">
+          <img src={icon} alt={alt} />
+          <div className="justify-start text-zinc-700 text-lg font-semibold font-['Pretendard'] leading-7">
+            {title}
+          </div>
+        </div>
+        <div className="w-6 h-6" />
+        <img src={trashBinGray} alt="휴지통" />
+      </div>
+    </div>
+  );
+}
